0b089ef6f253d98b0139a5002142c81d2728bd4f
FisRuRadJohn <56910809+FisRuRadJohn@users.noreply.github.com>
2019-10-30 11:54:25 +0100

    Pick ups, and score
    Now there is "coins", we get an score, and new features in the map (stairs).

---
+++ Assets/Scripts/Pickupcoin.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pickupcoin : MonoBehaviour
+{
+    public int points;
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 rotation = new Vector3(30, 40, 45) * Time.deltaTime;
+        transform.Rotate(rotation);
+    } 
+}
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,10 +1,14 @@
-﻿using UnityEngine;
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
 
     public Rigidbody myRigidBody;
     public float speed;
+    int score=0;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -20,12 +24,27 @@ public class PlayerController : MonoBehaviour
     private void FixedUpdate()
     {
         float movementHorizontal = Input.GetAxis("Horizontal"); //Declara la variable y le asigna el valor del eje horizontal
-        Debug.Log(movementHorizontal); //muestra en la ventana de comandos el valor 
+         
         float movementVertical = Input.GetAxis("Vertical");
-        Debug.Log(movementVertical);
+        
 
         Vector3 movement = new Vector3(movementHorizontal, 0f, movementVertical);
         myRigidBody.AddForce(speed * movement);
         
+    }
+
+    private void OnTriggerEnter(Collider other)  // para los objetos que tengan triger pasa lo de abajo cuando lo atravesamos
+    {
+        if (other.gameObject.CompareTag("PickUp")) //para que solo pase el tema de desaparecer con los objetos que tengan el tag que le he concretado
+        {
+            other.gameObject.SetActive(false);  //hace que desaparezca el objeto cuando colisiona si tiene trigger
+
+            score ++;
+
+            score += other.gameObject.GetComponent<Pickupcoin>().points;
+
+            Debug.Log("Mi puntuacion es : " + score);
+
+        }
     }
 }
