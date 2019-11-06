26837c6f723a5b8f99bc1ef040889cec0e7413c9
FisRuRadJohn <56910809+FisRuRadJohn@users.noreply.github.com>
2019-11-06 12:04:17 +0100

    Pre Alpha Update
    Camera follows the ball and we display the current score on UI

---
+++ Assets/Scripts/Camera.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+public class Camera : MonoBehaviour
+{
+    public GameObject target;
+
+    Vector3 offset;
+
+  
+    void Start()
+    {
+        offset = transform.position - target.transform.position;
+    }
+
+   
+    void Update()
+    {
+        transform.position = target.transform.position + offset;
+    }
+}
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,18 +1,22 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PlayerController : MonoBehaviour
 {
 
     public Rigidbody myRigidBody;
     public float speed;
-    int score=0;
+    public Text scoreText;
+
+    int score = 0;
 
     // Start is called before the first frame update
     void Start()
     {
         Debug.Log("Hello world!!!");
+        scoreText.text = "SCORE :" + 0;
     }
 
     // Update is called once per frame
@@ -33,17 +37,18 @@ public class PlayerController : MonoBehaviour
         
     }
 
-    private void OnTriggerEnter(Collider other)  // para los objetos que tengan triger pasa lo de abajo cuando lo atravesamos
+    private void OnTriggerEnter(Collider other)  // para los objetos que tengan triger, pasa lo de abajo cuando lo atravesamos
     {
         if (other.gameObject.CompareTag("PickUp")) //para que solo pase el tema de desaparecer con los objetos que tengan el tag que le he concretado
         {
-            other.gameObject.SetActive(false);  //hace que desaparezca el objeto cuando colisiona si tiene trigger
+            other.gameObject.SetActive(false);  //hace que desaparezca el objeto cuando colisiona si tiene trigger   , si es un pick up, lo desactivo.
 
-            score ++;
 
             score += other.gameObject.GetComponent<Pickupcoin>().points;
 
-            Debug.Log("Mi puntuacion es : " + score);
+            
+
+            scoreText.text = "SCORE :" + score;
 
         }
     }
